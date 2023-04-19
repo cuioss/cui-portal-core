c9d8f5b37d29fb2720cb78a13bedbbf4c60f60ab
Oliver Wolff <23139298+cuioss@users.noreply.github.com>
2023-04-19 16:19:27 +0200

    Moving tomcat modules

--- modules/authentication/portal-authentication-servlet-container/src/test/java/de/cuioss/portal/authentication/servlet/ModuleConsistencyTest.java
+++
@@ -1,7 +0,0 @@
-package de.cuioss.portal.authentication.servlet;
-
-import de.cuioss.portal.core.test.tests.BaseModuleConsistencyTest;
-
-class ModuleConsistencyTest extends BaseModuleConsistencyTest {
-
-}
