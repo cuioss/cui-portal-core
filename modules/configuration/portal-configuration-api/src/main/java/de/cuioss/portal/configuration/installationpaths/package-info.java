b50aff833f4d652a2a3f1a80de2051a81567a931
Oliver Wolff <23139298+cuioss@users.noreply.github.com>
2024-06-21 15:14:29 +0200

    Removing PortalInstallationPath and friends

--- modules/configuration/portal-configuration-api/src/main/java/de/cuioss/portal/configuration/installationpaths/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Members of this package provide Runtime Configuration about installation
- * specific paths of a portal application.
- *
- * @author Oliver Wolff
- *
- */
-package de.cuioss.portal.configuration.installationpaths;
